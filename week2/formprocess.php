d1c5c2ed44221e92f415a6141529428e76e6e716
Lam Nguyen <lam@impl.vn>
2020-03-10 10:52:27 +0700

    add week2 ex1

--- week2/formprocess.php
+++ week2/formprocess.php
@@ -1,27 +1,45 @@
-<?php
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-$age = 6;
-print("My age is $age!");
-?>
-
+<!DOCTYPE html>
 <html>
     <head>
-        <title>title</title>
+        <title>User Infomation</title>
+        <style>
+            :root {
+                box-sizing: border-box;
+            }
+            
+            *,
+            :before,
+            :after {
+                box-sizing: inherit;
+            }
+            
+            body {
+                font-family: Helvetica;
+                font-size: 18px;
+            }
+            
+            .container {
+                max-width: 1080px;
+                margin: auto;
+            }
+        </style>
     </head>
     <body>
-        <?php
-        $first_num = 1;
-        $second_num = 2;
-        $temp = $first_num;
-        $first_num = $second_num;
-        $second_num = $temp;
-
-        print("first_num = $first_num, second_num = $second_num!");
-        ?>
+        <div class="container">
+            <header>
+                <h1>Hello<?php if(isset($_POST['name'])) echo ', ' . $_POST['name'];?></h1>
+            </header>
+            <main>
+                <div class="content">
+                    <div class="user-info">
+                        <h2>Your full information</h2>
+                        <p>Name: <?php if(isset($_POST['name'])) echo $_POST['name'];?></p>
+                        <p>Age: <?php if(isset($_POST['age'])) echo $_POST['age'];?></p>
+                        <p>Gender: <?php if(isset($_POST['gender'])) echo $_POST['gender'];?></p>
+                        <p>Class: <?php if(isset($_POST['class'])) echo $_POST['class'];?></p>
+                    </div>
+                </div>
+            </main>
+        </div>
     </body>
 </html>
-
