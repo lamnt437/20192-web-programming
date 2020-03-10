e9c85f0ad8fe352217cdda50d0322bf44d80cb93
Lam Nguyen <lam@impl.vn>
2020-03-10 09:42:59 +0700

    init week2

---
+++ week2/formprocess.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$age = 6;
+print("My age is $age!");
+?>
+
+<html>
+    <head>
+        <title>title</title>
+    </head>
+    <body>
+        <?php
+        $first_num = 1;
+        $second_num = 2;
+        $temp = $first_num;
+        $first_num = $second_num;
+        $second_num = $temp;
+
+        print("first_num = $first_num, second_num = $second_num!");
+        ?>
+    </body>
+</html>
+
